b30c673ca9127921af0791e52bffeeb6d85263b8
theslothbear <128232763+theslothbear@users.noreply.github.com>
2024-11-05 21:48:47 +0500

    Update 051124.py

--- 051124.py
+++ 051124.py
@@ -10,3 +10,30 @@ for i in range(len(s)):
 		max_count = max(count, max_count)
 		count = 0
 print(max_count)
+
+'''
+В текстовом файле k7a-1.txt находится цепочка из символов латинского алфавита A, B, C, D, E. Найдите длину самой длинной подцепочки, состоящей из символов A, B или C (в произвольном порядке).
+'''
+s = open('k7a-1.txt', 'r').readline()
+count, max_count = 0, 0
+for i in range(len(s)):
+	if s[i] in ['A', 'B', 'C']:
+		count+=1
+	else:
+		max_count = max(count, max_count)
+		count = 0
+print(max_count)
+
+'''
+В текстовом файле k7b-2.txt находится цепочка из символов латинского алфавита A, B, C, D, E, F. Найдите максимальную длину цепочки вида DBACDBACDBAC.... (состоящей из фрагментов DBAC, последний фрагмент может быть неполным).
+'''
+s = open('k7b-2.txt', 'r').readline()
+s2 = 'DBAC' * (len(s)//4 + 1)
+count, max_count = 0, 0
+for i in range(len(s)):
+	if s[i] == s2[count]:
+		count+=1
+	else:
+		max_count = max(count, max_count)
+		count = 0
+print(max_count)
