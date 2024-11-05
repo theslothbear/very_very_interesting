633f15be8715e6ad9919eccf10d05784de1e382e
theslothbear <128232763+theslothbear@users.noreply.github.com>
2024-11-05 21:59:37 +0500

    Update 051124.py

--- 051124.py
+++ 051124.py
@@ -40,3 +40,19 @@ for i in range(len(s)):
 		max_count = max(count, max_count)
 		count = 0
 print(max_count)
+
+'''
+В текстовом файле k7c-2.txt находится цепочка из символов латинского алфавита A, B, C, D, E, F. Найдите количество цепочек длины 3, удовлетворяющих следующим условиям: 
+•	1-й символ – один из A, C, E; 
+•	2-й символ – один из A, D, F, который не совпадает с первым; 
+•	3-й символ – один из A, B, F, который не совпадает со вторым.
+
+'''
+s = open('k7c-2.txt', 'r').readline()
+count = 0
+for i in range(len(s)-2):
+	c = s[i:i+3]
+	if c[0] in ['A', 'C', 'E'] and c[1] in ['A', 'D', 'F'] and c[1] != c[0] and c[2] in ['A', 'B', 'F'] and c[2] != c[1]:
+		count+=1
+print(count)
+
